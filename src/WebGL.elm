5915f5b3be393cd673d00f9bcf860e294b2e6122
Evan Czaplicki <info@elm-lang.org>
2014-12-06 16:29:55 -0800

    Update syntax for 0.14

--- src/WebGL.elm
+++ src/WebGL.elm
@@ -27,6 +27,7 @@ import Http (Response)
 import Native.WebGL
 import Signal (Signal)
 
+
 {-| Triangles are the basic building blocks of a mesh. You can put them together
 to form any shape. Each corner of a triangle is called a *vertex* and contains a
 bunch of *attributes* that describe that particular corner. These attributes can
@@ -35,23 +36,30 @@ be things like position and color.
 So when you create a `Triangle` you are really providing three sets of attributes
 that describe the corners of a triangle.
 -}
-type Triangle attributes = (attributes, attributes, attributes)
+type alias Triangle attributes =
+    (attributes, attributes, attributes)
+
 
 {-| Apply a function to each vertex. This lets you transform the set of
 attributes associated with each corner of a triangle.
 -}
 mapTriangle : (a -> b) -> Triangle a -> Triangle b
-mapTriangle f (x,y,z) = (f x, f y, f z)
+mapTriangle f (x,y,z) =
+    (f x, f y, f z)
+
 
 {-| Combine two triangles by putting each of their vertices together with
 a given function.
 -}
 zipTriangle : (a -> b -> c) -> Triangle a -> Triangle b -> Triangle c
-zipTriangle f (x,y,z) (x',y',z') = (f x x', f y y', f z z')
+zipTriangle f (x,y,z) (x',y',z') =
+    (f x x', f y y', f z z')
+
 
 {-| Shader is a phantom data type. Don't instantiate it yourself. See below.
 -}
-data Shader attributes uniforms varyings = Shader
+type Shader attributes uniforms varyings = Shader
+
 
 {-| Shaders are programs for running many computations on the GPU in parallel.
 They are written in a language called
@@ -65,17 +73,23 @@ GLSL. It is intended specifically for libary writers who want to create shader
 combinators.
 -}
 unsafeShader : String -> Shader attribute uniform varying
-unsafeShader = Native.WebGL.unsafeCoerceGLSL
+unsafeShader =
+  Native.WebGL.unsafeCoerceGLSL
 
-data Texture = Texture
+
+type Texture = Texture
+
 
 {-| Loads a texture from the given url. PNG and JPEG are known to work, but
 other formats have not been as well-tested yet.
 -}
 loadTexture : String -> Signal (Response Texture)
-loadTexture = Native.WebGL.loadTex
+loadTexture =
+  Native.WebGL.loadTex
 
-data Entity = Entity 
+
+type Entity = Entity 
+
 
 {-| Packages a vertex shader, a fragment shader, a mesh, and uniform variables
 as an `Entity`. This specifies a full rendering pipeline to be run on the GPU.
@@ -86,12 +100,15 @@ Values will be cached intelligently, so if you have already sent a shader or
 mesh to the GPU, it will not be resent. This means it is fairly cheap to create
 new entities if you are reusing shaders and meshes that have been used before.
 -}
-entity : Shader attributes uniforms varyings -> Shader {} uniforms varyings -> [Triangle attributes] -> uniforms -> Entity
-entity = Native.WebGL.entity
+entity : Shader attributes uniforms varyings -> Shader {} uniforms varyings -> List (Triangle attributes) -> uniforms -> Entity
+entity =
+  Native.WebGL.entity
+
 
 {-| Render a WebGL scene with the given dimensions and entities. Shaders and
 meshes are cached so that they do not get resent to the GPU, so it should be
 relatively cheap to create new entities out of existing values.
 -}
-webgl : (Int,Int) -> [Entity] -> Element
-webgl = Native.WebGL.webgl
+webgl : (Int,Int) -> List Entity -> Element
+webgl =
+  Native.WebGL.webgl
