df0c4a062638c53404cace23f4128ec7f011b7fb
Alexander Foremny <aforemny@posteo.de>
2015-06-01 12:15:40 +0200

    Default texture filters to GL_NEAREST
    If you want filtering you'll have to write it yourself in the fragment
    shader.

--- src/Native/WebGL.js
+++ src/Native/WebGL.js
@@ -61,8 +61,8 @@ Elm.Native.WebGL.make = function(elm) {
     gl.bindTexture(gl.TEXTURE_2D, tex);
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
     gl.generateMipmap(gl.TEXTURE_2D);
     //gl.bindTexture(gl.TEXTURE0, null);
     return tex;
